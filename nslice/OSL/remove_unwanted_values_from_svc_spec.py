e4e9c522f7c7c1c1453368ac58ca17c6e295352d
Rafael Direito <rdireito.av.it.pt>
2024-10-09 11:57:42 +0000

    added new service specifications for OSL

---
+++ nslice/OSL/remove_unwanted_values_from_svc_spec.py
@@ -0,0 +1,29 @@
+import json
+
+input_file = 'ServiceCreationPayload-3GPP-NetSlice-Adapted4ITAv.json'
+output_file = input_file.split(".json")[0] + "-NoValues.json"
+
+# Open and read the JSON file
+with open(input_file, 'r') as file:
+    data = json.load(file)
+
+    for characteristic in data["serviceSpecCharacteristic"]:
+        value_type = characteristic["valueType"]
+
+        if value_type != "ENUM":
+            # Remove Characteristic Values
+            characteristic["serviceSpecCharacteristicValue"] = []
+        else:
+            # Remove repeaded values
+            initial_values = characteristic["serviceSpecCharacteristicValue"]
+            # Check for repetitions
+            values = {}
+            for initial_value in initial_values:
+                 values[initial_value["value"]["value"]] = initial_value
+            # Update without repeated elements
+            characteristic["serviceSpecCharacteristicValue"] = list(values.values())
+
+    
+    with open(output_file, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
